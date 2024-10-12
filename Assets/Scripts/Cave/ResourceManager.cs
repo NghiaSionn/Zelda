a33c6ff978d6aa484826172dac18b416f0740826
thmap285 <dinhthaimap285@gmail.com>
2024-10-12 22:57:51 +0700

    Thêm ScriptableObject

---
+++ Assets/Scripts/Cave/ResourceManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    [Header("Resource settings")]
+    [SerializeField] private List<ResourceData> resourcesData;
+}
---
+++ Assets/Scripts/ScriptableObjects/ResourceData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ResourceData_")]
+public class ResourceData : ScriptableObject
+{
+    public GameObject prefab;
+    public float chance;
+}

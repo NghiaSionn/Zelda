b9274c4da6cdc971fb7e6f40efad9fdb4fa78818
thmap285 <dinhthaimap285@gmail.com>
2024-10-18 19:31:04 +0700

    Thêm hàm để tạo ra các lối đi

--- Assets/Scripts/Cave/CaveManager.cs
+++ Assets/Scripts/Cave/CaveManager.cs
@@ -16,6 +16,7 @@ public class CaveManager : MonoBehaviour
     [SerializeField] private int smoothIterations = 3;
     [SerializeField] private int minWallsToStayWall = 4; //2-3
     [SerializeField] private int minWallsToBecomeWall = 4; //4-5
+    [SerializeField] private int pathWidth = 1;
 
     [Header("Tile settings")]
     [SerializeField] private Tilemap floorsTilemap; //0
@@ -29,18 +30,83 @@ public class CaveManager : MonoBehaviour
     public VectorValue startingPosition;
 
     private int[,] caveMap;
+    private AreaManager areaManager;
     private GameObject currentBase;
     private GameObject currentPlayer;
 
     internal void GenerateMap()
     {
         caveMap = new int[width, height];
+        areaManager = GetComponent<AreaManager>();
 
         FillMap();
+
+        areaManager.Initialize(caveMap, width, height);
+        var areas = areaManager.GetAreas();
+        ConnectAreasWithPaths(areas);
 
         DrawMap();
     }
 
+    private void ConnectAreasWithPaths(List<List<Vector2Int>> areas)
+    {
+        for (int i = 0; i < areas.Count - 1; i++)
+        {
+            Vector2Int pointA = areas[i][Random.Range(0, areas[i].Count)];
+            Vector2Int pointB = areas[i + 1][Random.Range(0, areas[i + 1].Count)];
+
+            FillPath(pointA, pointB);
+        }
+    }
+
+    private void FillPath(Vector2Int start, Vector2Int end)
+    {
+        Vector2Int currentPos = start;
+
+        while (currentPos != end)
+        {
+            if (Random.value < 0.2f) // zigzag
+            {
+                if (Random.value < 0.5f)
+                    currentPos.x += (Random.value < 0.5f) ? 1 : -1;
+                else
+                    currentPos.y += (Random.value < 0.5f) ? 1 : -1;
+            }
+            else
+            {
+                if (Random.value < 0.5f)
+                {
+                    if (currentPos.x < end.x)
+                        currentPos.x++;
+                    else if (currentPos.x > end.x)
+                        currentPos.x--;
+                }
+                else
+                {
+                    if (currentPos.y < end.y)
+                        currentPos.y++;
+                    else if (currentPos.y > end.y)
+                        currentPos.y--;
+                }
+            }
+
+            caveMap[currentPos.x, currentPos.y] = 0;
+
+            for (int offsetX = -pathWidth; offsetX <= pathWidth; offsetX++)
+            {
+                for (int offsetY = -pathWidth; offsetY <= pathWidth; offsetY++)
+                {
+                    int newX = currentPos.x + offsetX;
+                    int newY = currentPos.y + offsetY;
+
+                    if (IsInMapRange(newX, newY) && !IsMapBorder(newX, newY))
+                    {
+                        caveMap[newX, newY] = 0;
+                    }
+                }
+            }
+        }
+    }
     private void DrawMap()
     {
         floorsTilemap.ClearAllTiles();

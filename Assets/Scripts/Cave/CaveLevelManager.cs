8b54d433cfa7024cdf1b7861216f35d5336c21ba
thmap285 <dinhthaimap285@gmail.com>
2024-10-29 13:59:25 +0700

    Update CaveLevelManager.cs

--- Assets/Scripts/Cave/CaveLevelManager.cs
+++ Assets/Scripts/Cave/CaveLevelManager.cs
@@ -11,10 +11,12 @@ public class CaveLevelManager : MonoBehaviour
 {
     [SerializeField] private CaveManager caveManager;
     [SerializeField] private OreManager oreManager;
+    [SerializeField] private CaveEnemyManager caveEnemyManager;
     [SerializeField] private SceneTransitions sceneTransitions;
 
     private Dictionary<int, int[,]> caveMapDicts = new();
     internal Dictionary<int, Dictionary<Vector2, OreData>> oreDataDicts = new();
+    internal Dictionary<int, Dictionary<Vector2, GameObject>> enemyDicts = new();
     private Dictionary<int, (Vector3 stairUp, Vector3 stairDown)> stairPositionDicts = new();
 
     internal int currentLevel = 1;
@@ -60,6 +62,7 @@ public class CaveLevelManager : MonoBehaviour
         {
             caveManager = FindObjectOfType<CaveManager>();
             oreManager = FindObjectOfType<OreManager>();
+            caveEnemyManager = FindObjectOfType<CaveEnemyManager>();
             sceneTransitions = FindObjectOfType<SceneTransitions>();
 
             GenerateLevel(currentLevel);
@@ -83,17 +86,20 @@ public class CaveLevelManager : MonoBehaviour
         if (!caveMapDicts.ContainsKey(level) && !oreDataDicts.ContainsKey(level) && !stairPositionDicts.ContainsKey(level))
         {
             caveManager.GenerateMap();
-            oreManager.GenerateOre(level, false);
+            oreManager.GenerateOres(level, false);
+            caveEnemyManager.GenerateEnemies(level);
 
             caveMapDicts[level] = caveManager.GetMap();
             stairPositionDicts[level] = caveManager.GetStairsPosition();
             oreDataDicts[level] = oreManager.GetOres();
+            enemyDicts[level] = caveEnemyManager.GetEnemies();
         }
         else
         {
             caveManager.LoadMap(caveMapDicts[level]);
             caveManager.SetStairsPosition(stairPositionDicts[level]);
             oreManager.LoadOres(oreDataDicts[level]);
+            caveEnemyManager.LoadEnemies(enemyDicts[level]);
         }
 
         OnLevelChanged?.Invoke();

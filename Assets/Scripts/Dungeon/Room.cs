195950e307bc13c03aaf58da06c88cc4cf18f842
thmap285 <dinhthaimap285@gmail.com>
2024-11-01 12:58:14 +0700

    Update Room.cs

--- Assets/Scripts/Dungeon/Room.cs
+++ Assets/Scripts/Dungeon/Room.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum RoomType
+{
+    Start,
+    Normal,
+    Boss,
+    Treasure
+}
+
 [System.Serializable]
 public class Room
 {
     public Vector2Int position;
     public int width;
     public int height;
+    public RoomType type;
+    public List<Vector2Int> connectedDoors = new();
+    public Dictionary<Vector2Int, Room> neighbors = new();
 
-    public Room(Vector2Int position, int width, int height)
+    public Room(Vector2Int position, int width, int height, RoomType type = RoomType.Normal)
     {
         this.position = position;
         this.width = width;
         this.height = height;
+        this.type = type;
+    }
+
+    public bool CanConnectInDirection(Vector2Int direction)
+    {
+        return !connectedDoors.Contains(direction);
     }
 }
-

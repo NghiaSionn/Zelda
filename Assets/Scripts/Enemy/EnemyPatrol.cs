4a48cb51f6c968ecd29e3a5f6385951afcfabb56
NghiaSionn <ptcn0120223@gmail.com>
2024-11-05 16:10:46 +0700

    enemy patrol

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolLog : Log
+{
+    [Header("Điểm đi tuần ")]
+    public Transform[] path;
+
+    public int currentPoint;
+    public Transform currentGoal;
+    public float roundingDistance;
+
+
+    public override void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRaidus
+            && Vector3.Distance(target.position, transform.position) > attackRadius)
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk
+                && currentState != EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position,
+                                                   target.position,
+                                                   moveSpeed * Time.deltaTime);
+
+                changeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+                //ChangeState(EnemyState.walk);
+                anim.SetBool("wakeUp",
+ true);
+            }
+        }
+        else if (Vector3.Distance(target.position, 
+                        transform.position) > chaseRaidus)
+        {
+            if (Vector3.Distance(transform.position, 
+                        path[currentPoint].position) > roundingDistance)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position,
+                                                   path[currentPoint].position,
+                                                   moveSpeed * Time.deltaTime);
+                changeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+            }
+            else
+            {
+                ChangeGoal();
+            }
+        }
+    }
+
+    private void ChangeGoal()
+    {
+        if (currentPoint == path.Length - 1)
+        {
+            currentPoint = 0;
+            currentGoal = path[0];
+
+        }
+        else
+        {
+            currentPoint++;
+            currentGoal = path[currentPoint];
+        }
+    }
+}

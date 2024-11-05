44ca8858ec7ed48d3a542fc87f92b2f95542b8f4
thmap285 <dinhthaimap285@gmail.com>
2024-11-05 20:21:05 +0700

    Update Projectile.cs

--- Assets/Scripts/Enemy/Projectile.cs
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +9,14 @@ public class Projectile : MonoBehaviour
     public bool isBound = false;
     public float lifeTime = 5f;
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
-        Destroy(gameObject, lifeTime);
+        StartCoroutine(AutoDestroy());
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,10 +28,25 @@ public class Projectile : MonoBehaviour
                 Vector2 normal = ((Vector2)transform.position - other.ClosestPoint(transform.position)).normalized;
                 rb.velocity = Vector2.Reflect(rb.velocity, normal);
             }
-            else
-            {
-                Destroy(gameObject);
-            }
         }
+        if(other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(DestroyProjectile());
+        }
+    }
+
+    private IEnumerator AutoDestroy()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        StartCoroutine(DestroyProjectile());
+    }
+
+    private IEnumerator DestroyProjectile()
+    {
+        rb.velocity = Vector2.zero;
+        animator.SetTrigger("Destroy");
+
+        yield return new WaitForSeconds(0.25f);
+        Destroy(gameObject);
     }
 }

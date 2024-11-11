846374cab7fb97d21fd8058fcc8b86e129404184
Phạm Trần Chung Nghĩa <ptcn0120223@gmail.com>
2024-11-11 11:43:51 +0700

    fix chest house village

--- Assets/Scripts/Skill/Projectile.cs
+++ Assets/Scripts/Skill/Projectile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,15 +12,19 @@ public class Projectile : MonoBehaviour
     private Vector2 direction;
     private Animator animator;
 
+    private CinemachineImpulseSource impulseSource;
+
     public void SetDirection(Vector2 dir)
     {
         direction = dir;
+        
     }
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        impulseSource = GetComponent<CinemachineImpulseSource>();
     }
 
     void Update()
@@ -30,15 +35,17 @@ public class Projectile : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        
-        if (other.gameObject.CompareTag("enemy") ||  other.gameObject.CompareTag("Tree"))
+        if (other.gameObject.CompareTag("breakable"))
         {
-            
             Destroy(gameObject);
+            other.gameObject.GetComponent<Pot>().Smash();
         }
 
-        if(other.gameObject.CompareTag("Interactive"))
+
+        if (other.gameObject.CompareTag("Interactive") || other.gameObject.CompareTag("Tree") 
+            || other.gameObject.CompareTag("House") || other.gameObject.CompareTag("enemy"))
         {
+            CameraShakeManager.instance.CameraShake(impulseSource);
             Destroy(gameObject);
         }
     }

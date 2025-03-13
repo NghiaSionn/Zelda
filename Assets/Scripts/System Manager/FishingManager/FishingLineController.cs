67e66c1ee54836347296212b00d2ad48d749834e
Phạm Trần Chung Nghĩa <ptcn0120223@gmail.com>
2025-03-13 21:24:59 +0700

    update animation fishing + add fishing line

---
+++ Assets/Scripts/System Manager/FishingManager/FishingLineController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class FishingLineController : MonoBehaviour
+{
+    public Transform[] rodTips;
+    public Transform[] hooks; 
+    public int currentDirection = 0; // Hướng hiện tại (0: Up, 1: Down, 2: Left, 3: Right)
+
+    public TrailRenderer trailRenderer;
+
+    void Start()
+    {
+        DisableLine();
+        trailRenderer = GetComponentInChildren<TrailRenderer>();
+
+        // Tìm tất cả các hook trong game theo tag "Hook"
+        GameObject[] hookObjects = GameObject.FindGameObjectsWithTag("Hook");
+
+        // Lấy transform của từng hook
+        hooks = new Transform[hookObjects.Length];
+        for (int i = 0; i < hookObjects.Length; i++)
+        {
+            hooks[i] = hookObjects[i].transform;
+        }
+    }
+
+    void Update()
+    {
+        if (hooks.Length == 0) return;
+
+        // Chọn điểm neo dựa trên hướng hiện tại
+        Transform currentRodTip = rodTips[currentDirection];
+
+        // Cập nhật vị trí dây câu cho mỗi hook
+        foreach (var hook in hooks)
+        {
+            transform.position = (currentRodTip.position + hook.position) / 2f;
+            transform.LookAt(hook.position);
+        }
+    }
+
+    public void ChangeDirection(int direction)
+    {
+        currentDirection = direction;
+    }
+
+    public void ClearLine()
+    {
+        trailRenderer.Clear();
+    }
+
+    public void ActiveLine()
+    {
+        trailRenderer.gameObject.SetActive(true);
+    }
+
+    public void DisableLine()
+    {
+        trailRenderer.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/System Manager/FishingManager/FishingZone.cs
+++ Assets/Scripts/System Manager/FishingManager/FishingZone.cs
@@ -1,8 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting.Antlr3.Runtime;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class FishingZone : Interactable
@@ -15,66 +12,49 @@ public class FishingZone : Interactable
     public FishingMiniGame fishMinigame;
 
     [Header("Loot khi câu cá")]
-    public LootBagData fishingLootData; 
+    public LootBagData fishingLootData;
     public LootUIManager lootUIManager;
 
     public PlayerMovement playerMovement;
+    private FishingLineController fishingLineController;
 
     public bool isFishing;
     private bool canFish = false;
-    // Start is called before the first frame update
+
     void Awake()
     {
         fishingMiniGame.SetActive(false);
         lootUIManager = FindObjectOfType<LootUIManager>();
-
+        fishingLineController = FindAnyObjectByType<FishingLineController>();
 
         if (playerMovement == null)
         {
-            playerMovement = FindObjectOfType<PlayerMovement>(); 
+            playerMovement = FindObjectOfType<PlayerMovement>();
         }
-
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
-    public void Fishing()
-    {
-        fishMinigame.ResetGame();    
-        isFishing = true;
-        StartCoroutine(IsFishing());
-        Debug.Log("Fishing: " + playerAnimator.GetBool("fishing"));
-    }
-
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && playerInRange && !isFishing)
         {
-            if (Input.GetKeyDown(KeyCode.E) && playerInRange && !isFishing)
-            {
-                Debug.Log("Bắt đầu câu cá");
-                Fishing();
-            }
-            else if (isFishing)
-            {
-                Debug.Log("Đang câu cá, không thể bắt đầu mới.");
-            }
+            Debug.Log("Bắt đầu câu cá");
+            StartCoroutine(StartFishingWithDelay());
         }
     }
 
-    IEnumerator IsFishing()
+    IEnumerator StartFishingWithDelay()
     {
-
-        playerMovement.enabled = false;
-
         isFishing = true;
-        playerAnimator.SetBool("fishing", true);
+        playerMovement.enabled = false;
+        playerAnimator.SetTrigger("startfishing");
+        fishingLineController.ActiveLine();
+
+        // Random thời gian chờ từ 5 - 20 giây
+        float waitTime = Random.Range(5f, 20f);
+        yield return new WaitForSeconds(waitTime);
+
         fishingMiniGame.SetActive(true);
+        fishMinigame.ResetGame();
 
         yield return new WaitUntil(() => fishMinigame.IsComplete);
 
@@ -85,16 +65,13 @@ public class FishingZone : Interactable
             GenerateLoot();
         }
 
-        EndFishing();
-
-
+        StartCoroutine(EndFishing());
     }
 
     private void GenerateLoot()
     {
         int lootCount = Random.Range(fishingLootData.lootCountRange.x, fishingLootData.lootCountRange.y + 1);
 
-        // Tạo danh sách tạm thời và shuffle
         List<LootBagData.LootItem> tempItems = new List<LootBagData.LootItem>(fishingLootData.lootItems);
         tempItems.Shuffle();
 
@@ -104,26 +81,21 @@ public class FishingZone : Interactable
         {
             LootBagData.LootItem lootItem = tempItems[i];
             int quantity = Random.Range(lootItem.quantityRange.x, lootItem.quantityRange.y + 1);
-
             lootItem.quantity = quantity;
-
             selectedLoot.Add(lootItem);
-
             Debug.Log($"Nhặt được {quantity} {lootItem.item.itemName}.");
-
-            playerInventory.AddItem(lootItem.item, quantity);           
+            playerInventory.AddItem(lootItem.item, quantity);
         }
 
         lootUIManager.DisplayLoot(selectedLoot);
     }
 
-    private void EndFishing()
+    IEnumerator EndFishing()
     {
         playerMovement.enabled = true;
         isFishing = false;
-        playerAnimator.SetBool("fishing", false);
+        playerAnimator.SetTrigger("endfishing");
+        fishingLineController.DisableLine();
+        yield return null;
     }
-
-    
-
 }

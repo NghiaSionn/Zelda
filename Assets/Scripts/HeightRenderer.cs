8a9006e424c772315f5ea7c9c4e4a0e280c9aae7
Phạm Trần Chung Nghĩa <167677781+NghiaSionn@users.noreply.github.com>
2025-10-03 18:12:44 +0700

    add new shader water

---
+++ Assets/Scripts/HeightRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightRenderer : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private float baseHeight = 0f;   // cao mặc định
+    private float currentHeight = 0f; // thay đổi khi vào nước
+
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        // Công thức classic: -y + height
+        int order = Mathf.RoundToInt(-(transform.position.y + currentHeight) * 100);
+        sprite.sortingOrder = order;
+    }
+
+    public void SetHeight(float h)
+    {
+        currentHeight = h;
+    }
+
+    // Ví dụ khi vào vùng nước
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Water"))
+        {
+            SetHeight(-0.3f); // chìm 0.3 đơn vị
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Water"))
+        {
+            SetHeight(0f); // trở lại bình thường
+        }
+    }
+}
